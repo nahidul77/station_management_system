6cfdb64cd3b7ce2500706b6050bacbe4109d65e2
nahidul77 <nahidul.mee@gmail.com>
2022-08-15 21:05:30 +0600

    logo form added

--- application/views/pages/logo_form.php
+++ application/views/pages/logo_form.php
@@ -2,9 +2,7 @@
     <div class="panel panel-bd">
         <div class="panel-heading">
             <div class="panel-title">
-                <h4>
-                    <?php echo display('updatelogo'); ?>
-                </h4>
+                <h4>Update Logo</h4>
             </div>
         </div>
 
@@ -22,7 +20,7 @@
         <?php echo form_open_multipart('logo/update', array("class" => 'form-horizontal', 'id' => 'notice-submit')); ?>
         <div class="panel-body">
             <div class="form-group">
-                <label class="control-label col-xs-12 col-sm-3 no-padding-right" for="d_emergency_cell"><?php echo display('logo'); ?></label>
+                <label class="control-label col-xs-12 col-sm-3 no-padding-right" for="d_emergency_cell">Logo</label>
                 <div class="col-xs-12 col-sm-9">
                     <div class="clearfix">
                         <input type="file" name="d_picture" id="v_fuel_rate" class="col-xs-12 col-sm-4 form-control" />
@@ -32,7 +30,7 @@
             </div>
 
             <div class="form-group">
-                <label class="control-label col-xs-12 col-sm-3 no-padding-right" for="d_emergency_cell"><?php echo display('favicon'); ?></label>
+                <label class="control-label col-xs-12 col-sm-3 no-padding-right" for="d_emergency_cell">Favicon</label>
                 <div class="col-xs-12 col-sm-9">
                     <div class="clearfix">
                         <input type="file" name="f_picture" id="v_fuel_rate" class="col-xs-12 col-sm-4 form-control" />
@@ -43,8 +41,8 @@
             <input name="logo_id" type="hidden" id="logo_id" value="<?php echo $logo[0]->logo_id; ?>" />
             <div class="form-group row">
                 <div class="col-md-offset-1 col-md-9" style="margin-left: 35%;">
-                    <button class="btn btn-danger w-md m-b-5"><?php echo display('cancel'); ?></button>
-                    <button type="submit" class="btn btn-success w-md m-b-5"><?php echo display('save'); ?></button>
+                    <button class="btn btn-danger w-md m-b-5">Cancel</button>
+                    <button type="submit" class="btn btn-success w-md m-b-5">Save</button>
                 </div>
             </div>
         </div>
